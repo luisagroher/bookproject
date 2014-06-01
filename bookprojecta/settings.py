feb75413664a4acdf867981848d71bad639eb3c7
unknown <luisagroher@gmail.com>
2014-06-01 13:38:01 -0400

    bookprojecta

--- bookprojecta/settings.py
+++ bookprojecta/settings.py
@@ -16,6 +16,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'a2#b6wo+c1e)ullfllj710lc@%!#8+mkw*@56p#1k%pscch(kb'
 
@@ -63,6 +67,11 @@ DATABASES = {
     }
 }
 
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_DIR, "templates"),
+    # here you can add another templates directory if you wish.
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -80,4 +89,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
+
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)

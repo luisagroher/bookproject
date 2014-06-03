eba30f9ef496b3a5f02882747ea21d4828697164
unknown <luisagroher@gmail.com>
2014-06-03 00:33:06 -0400

    test

--- bookprojecta/settings.py
+++ bookprojecta/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.staticfiles.views.serve',
-    'django.views.static.serve',
+    #'django.views.static.serve',
     'book',
 )
 

8d315d0fda83e3352c96f829cfb07646e05caeb0
unknown <luisagroher@gmail.com>
2014-06-02 21:06:45 -0400

    add data and assets

--- bookprojecta/settings.py
+++ bookprojecta/settings.py
@@ -104,13 +104,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-MEDIA_ROOT = ''
+MEDIA_ROOT = 'static/images/'
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'assets'
 
 STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+ ('assets', os.path.join(os.getcwd(),'static/')),
+    #os.path.join(BASE_DIR, 'static'),
 )
--- bookprojecta/urls.py
+++ bookprojecta/urls.py
@@ -9,4 +9,15 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^book/', include('book.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    
+            # user auth urls
+    url(r'^sign_in/$', 'bookprojecta.views.login'),
+    url(r'^auth/$', 'bookprojecta.views.auth_view'),
+    url(r'^logout/$', 'bookprojecta.views.logout'),
+    url(r'^loggedin/$', 'bookprojecta.views.loggedin'),
+    url(r'^invalid/$', 'bookprojecta.views.invalid_login'),
+    url(r'^sign_up/$', 'bookprojecta.views.register_user'),
+    url(r'^sign_up_success/$', 'bookprojecta.views.register_success'),
+    url(r'^about/$', 'bookprojecta.views.about'),
+    url(r'^contact/$', 'bookprojecta.views.contact'),
 )
